ece517351306ee71bfd38a3b515813f897e1b006
Hiranika <h1r4n1k4@gmail.com>
2022-11-01 18:25:51 +0700

    Cancel ssh settings

--- build-armbian/common-files/rootfs/etc/custom_service/start_service.sh
+++ build-armbian/common-files/rootfs/etc/custom_service/start_service.sh
@@ -9,8 +9,8 @@ lasthinker_release_file="/etc/lasthinker-release"
 [[ "${FDT_FILE}" == "meson-sm1-skyworth-lb2004-a4091.dtb" ]] && modprobe btmtksdio 2>/dev/null
 
 # Start ssh service
-[[ -d "/var/run/sshd" ]] || mkdir -p -m0755 /var/run/sshd
-[[ -f "/etc/init.d/ssh" ]] && /etc/init.d/ssh start 2>/dev/null
+#[[ -d "/var/run/sshd" ]] || mkdir -p -m0755 /var/run/sshd
+#[[ -f "/etc/init.d/ssh" ]] && /etc/init.d/ssh start 2>/dev/null
 
 # Add custom log
 echo "[$(date +"%Y.%m.%d.%H%M")] Hello World..." >/tmp/lasthinker_start_service.log
